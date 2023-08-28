79501cb0128d00659643885c09ab82b6bc856255
Ahmed <balochsaeed888@gmail.com>
2023-08-28 22:26:28 +0500

    show images on gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,39 +1,37 @@
-"use client";
+import { CloudinaryImage } from "./cloudinary-image";
+import UploadButton from "./upload-button";
+import cloudinary from "cloudinary";
 
-import { CldUploadButton } from "next-cloudinary";
-import React from "react";
-import { UploadResult } from "../page";
-import { Button } from "@/components/ui/button";
+interface SearchResult {
+  public_id: string;
+}
 
-const GalleryPage = () => {
+const GalleryPage = async () => {
+  const results = (await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("created_at", "desc")
+    .max_results(10)
+    .execute()) as { resources: SearchResult[] };
+
   return (
-    <section className="flex justify-between ">
-      <h1 className="text-4xl font-bold"> Gallery</h1>
-      <Button asChild>
-        <div className="flex gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-            />
-          </svg>
-
-          <CldUploadButton
-            uploadPreset="t5abl3pi"
-            onUpload={(result: UploadResult) => {
-              //   setImageId(result.info.public_id)
-            }}
-          />
+    <section>
+      <div className=" flex flex-col gap-8">
+        <div  className="flex justify-between ">
+          <h1 className="text-4xl font-bold"> Gallery</h1>
+          <UploadButton/>
         </div>
-      </Button>
+        <div className="grid grid-cols-4 gap-4">
+          {results.resources.map((result) => (
+            <CloudinaryImage
+              key={result.public_id}
+              src={result.public_id}
+              width="400"
+              height="300"
+              alt="an image of something"
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/app/gallery/upload-button.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { CldUploadButton } from "next-cloudinary";
+import React from "react";
+import { UploadResult } from "../page";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+const UploadButton = () => {
+  const router = useRouter();
+
+  return (
+    <Button asChild>
+      <CldUploadButton
+        uploadPreset="t5abl3pi"
+        onUpload={(result: UploadResult) => {
+          //   setImageId(result.info.public_id)
+          setTimeout(() => {
+            console.log("Refresh");
+            router.refresh();
+          }, 1000);
+        }}
+      >
+        <div className="flex gap-2 items-center ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          Upload
+        </div>
+      </CldUploadButton>
+    </Button>
+  );
+};
+
+export default UploadButton;
